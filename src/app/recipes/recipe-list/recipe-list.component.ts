e690030b5a954792c496c2c2bc5b43f49b0a8e46
jimkuo <jim.kuo@servian.com>
2022-01-13 15:57:47 +1000

    L54

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'testing', 'https://live.staticflickr.com/7864/47044929942_fc84a2bccf_b.jpg')
+  ];
   
   constructor() { }
 
